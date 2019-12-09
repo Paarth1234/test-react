e797c4be4289e75bc17966b29ad2e624e827c07f
Paarth <paarth171548.cse@chitkara.edu.in>
2019-12-09 18:15:03 +0530

    Updates

---
+++ precache-manifest.cdc42e302913b6a4f0e9494983c70e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afaa0f52e5e5fe361f09ea471a934f4",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "6755278272d4822ef6c7",
+    "url": "/test-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0e1db8ce0e9aa2150136",
+    "url": "/test-react/static/js/2.364bf40f.chunk.js"
+  },
+  {
+    "revision": "6755278272d4822ef6c7",
+    "url": "/test-react/static/js/main.e9464971.chunk.js"
+  },
+  {
+    "revision": "065a276583310919cc14",
+    "url": "/test-react/static/js/runtime-main.16586f45.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-react/static/media/logo.25bf045c.svg"
+  }
+]);
